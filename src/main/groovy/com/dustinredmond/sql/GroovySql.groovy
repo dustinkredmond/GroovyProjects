7df948a71f30f31633da36830d3badcac90f95b9
dredmond <dustinkredmond@gmail.com>
2020-09-02 12:39:12 -0400

    Add more examples

--- src/main/groovy/com/dustinredmond/sql/GroovySql.groovy
+++ src/main/groovy/com/dustinredmond/sql/GroovySql.groovy
@@ -6,6 +6,8 @@ class GroovySql {
 
     static void main(String[] args) {
         createATable()
+        doAnInsertStatement()
+        doAnUpdateStatement()
         doASelectStatement()
 
     }
@@ -17,12 +19,24 @@ class GroovySql {
         sql.close()
     }
 
+    static def doAnInsertStatement() {
+        def params = [1]
+        def sql = getSql()
+        sql.executeInsert('insert into testing (id) values (?)', params)
+    }
+
     static def doASelectStatement() {
         def sql = getSql()
         sql.eachRow("select id from testing") {
             print it[0] // prints the id
         }
         sql.close()
+    }
+
+    static def doAnUpdateStatement() {
+        def sql = getSql()
+        def params = ["John", 1]
+        sql.executeUpdate("update testing set first_name = ? where id = ?", params)
     }
 
     private static Sql getSql() {
