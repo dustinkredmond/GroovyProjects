ad1ae881d5fe4e0dd7516061bbe40cfaa309105d
dredmond <dustinkredmond@gmail.com>
2020-09-02 14:30:13 -0400

    Add operator overloading example

--- src/main/groovy/com/dustinredmond/operator/TestOverloading.groovy
+++ src/main/groovy/com/dustinredmond/operator/TestOverloading.groovy
@@ -6,7 +6,8 @@ class TestOverloading {
         def obj1 = new TestObject(5)
         def obj2 = new TestObject(3)
         // calls the overloaded + operator of our TestObject class
-        println obj1 + obj2
+        obj1 + obj2
+        obj1 - obj2
     }
 
     static class TestObject {
@@ -22,6 +23,7 @@ class TestOverloading {
          */
         def plus(someObj) {
             this.val += someObj.getVal()
+            println "Adding ${someObj.getVal()} to ${this.val}"
         }
 
         /*
@@ -29,7 +31,77 @@ class TestOverloading {
          */
         def minus(someObj) {
             this.val -= someObj.getVal()
+            println "Subtracting ${someObj.getVal()} from ${this.val}"
         }
+
+        // overload * (multiplication)
+        def multiply(someObj) {
+            this.val *= someObj.getVal()
+            println "Multiplying ${someObj.getVal()} by ${this.val}"
+        }
+
+        // overload / (division)
+        def div(someObj) {
+            this.val /= someObj.getVal()
+            println "Dividing ${someObj.getVal()} by ${this.val}"
+        }
+
+        // overload %
+        def mod(someObj) {
+            println "Overloaded %"
+        }
+
+        // overload **
+        def power(someObj) {
+            println "Overloaded **"
+        }
+
+        // overload |
+        def or(someObj) {
+            
+        }
+
+        // overload &
+        def and(someObj) {}
+
+        // overload ^
+        def xor(someObj) {}
+
+        // overload as (eg. thisObj as String)
+        def asType(someObj) {}
+
+        // overload obj()
+        def call() {}
+
+        // overload obj[?]
+        def getAt(someIndex) {}
+
+        // overload obj[?] = someVal
+        def putAt(someIndex, someVal) {}
+
+        // overload in (e.g. someObj in someOtherObj)
+        def isCase(someObj) {}
+
+        // overload <<
+        def leftShift(someObj) {}
+
+        // overload >>
+        def rightShift(someObj) {}
+
+        // overload ++
+        def next() {}
+
+        // overload --
+        def previous() {}
+
+        // overload +someObj
+        def positive() {}
+
+        // overload -someObj
+        def negative() {}
+
+        // overload ~
+        def bitwiseNegate() {}
     }
 
 }
